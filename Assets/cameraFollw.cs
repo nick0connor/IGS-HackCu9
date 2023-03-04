6e9d47b87600f2e2e57e244d096a0941fb5119b3
Parkerack <caac2213@colorado.edu>
2023-03-04 16:56:34 -0700

    camera follow is from TESter

---
+++ Assets/cameraFollw.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class cameraFollw : MonoBehaviour
+{
+    // Start is called before the first frame update
+	
+	public GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("th");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position = new Vector3(player.transform.position.x, player.transform.position.y+2, player.transform.position.z - 10);
+    }
+}
