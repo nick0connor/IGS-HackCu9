8ef7fb486f73eb430af26745df595138e2b99482
appyappyappy <williamashcraft112@gmail.com>
2023-03-05 10:02:51 -0700

    Gobo fight no work

--- Assets/GoblinFight.cs
+++ Assets/GoblinFight.cs
@@ -11,10 +11,11 @@ public class GoblinFight : MonoBehaviour
 
     private bool goblinTurn, gameActive;
     public int goblinHealth = 5;
-    public int playerHealth = 10;
+    public int playerHealth;
 
     public GameObject stateSpace;
     public GameObject goblin, player, menu;
+    public GameManager gm;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,7 @@ public class GoblinFight : MonoBehaviour
         gameActive = true;
         StartCoroutine(Test(1));
         goblinHealth = 5;
-        playerHealth = 10;
+        playerHealth = gm.playerHealth;
     }
 
     // Update is called once per frame
@@ -52,6 +53,10 @@ public class GoblinFight : MonoBehaviour
                 goblinTurn = false;
                 showUI();
             }
+            //var speed = 1.0f; //how fast it shakes
+            //var amount = 100.0f; //how much it shakes
+            //Vector3 tr = goblin.transform.position;
+            //tr.x = Mathf.Sin(Time.time * speed) * amount; ;
         } 
 	}
 
@@ -130,20 +135,28 @@ public class GoblinFight : MonoBehaviour
 
     private void shakePlayer()
     {
-        Vector3 initPos = player.transform.position;
+    //    var speed = 1.0f; //how fast it shakes
+    //    var amount = 100.0f; //how much it shakes
+    //    Vector3 tr = player.transform.position;
+    //    tr.x = Mathf.Sin(Time.time * speed) * amount;
+        //Vector3 initPos = player.transform.position;
 
-        player.transform.Translate(Vector3.left * Time.deltaTime);
-        StartCoroutine(Test(0.1f));
-        player.transform.Translate(Vector3.right * Time.deltaTime * 2);
-        StartCoroutine(Test(0.1f));
-        player.transform.Translate(Vector3.left * Time.deltaTime);
+        //player.transform.Translate(Vector3.left * Time.deltaTime);
+        //StartCoroutine(Test(0.1f));
+        //player.transform.Translate(Vector3.right * Time.deltaTime * 2);
+        //StartCoroutine(Test(0.1f));
+        //player.transform.Translate(Vector3.left * Time.deltaTime);
 
-        player.transform.position = initPos;
+        //player.transform.position = initPos;
     }
 
     private void shakeGoblin()
     {
-
+        //var speed = 1.0f; //how fast it shakes
+        //var amount = 100.0f; //how much it shakes
+        //Vector3 tr = goblin.transform.position;
+        //tr.x = 5;
+        //    //Mathf.Sin(Time.time * speed) * amount;
     }
 
     public void showUI()
