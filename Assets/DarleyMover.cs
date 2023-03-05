96dc43d1794e1c555dd03d5ce9f19a188f079ad1
appyappyappy <williamashcraft112@gmail.com>
2023-03-04 23:26:04 -0700

    Animation

--- Assets/DarleyMover.cs
+++ Assets/DarleyMover.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 public class DarleyMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public SpriteRenderer spriteRenderer;
+    public Animator ani;
+    bool moving;
+
     //private bool stuck;
 
     // Start is called before the first frame update
     void Start()
     {
         //stuck = false;
+        spriteRenderer.flipX = false;
+        moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        moving = false;
         if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
             this.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             this.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            spriteRenderer.flipX = true;
             this.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            spriteRenderer.flipX = false;
             this.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            moving = true;
         }
-
+        ani.SetBool("Moving", moving);
     }
 
     void OnCollisionEnter2D(Collision2D col)
