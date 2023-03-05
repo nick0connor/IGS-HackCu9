9017530dcf6d6ef7f725a306d064139e51c887e3
nick0connor <nioc4468@colorado.edu>
2023-03-05 00:27:24 -0700

    tomororw

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,6 +7,15 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance;
 
+    public int playerHealth = 10;
+    public int goblinsKilled = 0;
+    public int maxGobs = 3;
+
+    private bool[] dead = new bool[] {false, false, false};
+    public GameObject goblin1, goblin2, goblin3;
+
+    public GameObject level, winScreen;
+
     void Awake(){
         Instance = this;
     }
@@ -20,6 +29,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            killGoblin();
+        }
+    }
+
+    public void killGoblin()
+    {
+        goblinsKilled++;
         
+        if(goblinsKilled >= maxGobs)
+        {
+            level.SetActive(false);
+            winScreen.SetActive(true);
+        }
     }
 }
