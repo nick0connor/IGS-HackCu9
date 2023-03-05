9135dfa513f734b7230ab0cb77b6b98501c4ab2f
nick0connor <nioc4468@colorado.edu>
2023-03-04 22:11:07 -0700

    started work on fight scene, man walking

--- Assets/GoblinFight.cs
+++ Assets/GoblinFight.cs
@@ -1,42 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class GoblinFight : MonoBehaviour
 {
+
+    private bool goblinTurn;
+    private int goblinHealth, playerHealth;
+
+    public GameObject goblin, player, menu;
+
     // Start is called before the first frame update
     void Start()
     {
-       
-		
+        goblinTurn = false;
+        goblinHealth = 5;
+        playerHealth = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-		 bool goblinAttack=false;
-		bool fight=true;
-		int goblinHealth=1;
-		int playerHealth=10;
-		while(fight==true){
-		if(goblinHealth <=0 || playerHealth<=0){
-			fight=false;
-		}
-		if (Input.GetKey(KeyCode.E)) {
-			goblinAttack=true;
-		}
-        if(goblinAttack==true){
-			this.transform.Translate(Vector3.up* Time.deltaTime);
-			this.transform.Translate(Vector3.left*3* Time.deltaTime);
-			playerHealth--;
-			float j=Time.time;
-			for(float i=Time.time; j==(i+2);i++){	
-			}
-			this.transform.Translate(Vector3.right*3* Time.deltaTime);
-			goblinAttack=false;
-		}
+		if(goblinTurn)
+        {
+
+
+        } else
+        {
+
+
+        }
 		
-		
-		}
-}
+	}
+
+    public void showUI()
+    {
+        menu.SetActive(true);
+    }
+
+    public void hideUI()
+    {
+        menu.SetActive(false);
+    }
 }
